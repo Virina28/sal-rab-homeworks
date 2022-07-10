021378f17b51ded13ec0e3b0fd9880884ca75655
Virina28 <virina.rabota@gmail.com>
2022-07-10 16:49:29 +0300

    add

--- task_4/solution.js
+++ task_4/solution.js
@@ -3,6 +3,7 @@
 function parseProducts(json) {
     let data = JSON.parse(json);
     let products = data.products;
+console.log(products);
 
 function renderProductsCards(json) {// Напишите функцию renderProductsCards(json)
 // Аргументом функции является JSON
